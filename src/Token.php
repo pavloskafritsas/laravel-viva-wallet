336fc9a3c2271912b1a18d8958fcd8213ed33c6f
PK <pavkfr@hotmail.com>
2022-03-23 12:19:05 +0200

    fix(Token): refresh return type

--- src/Token.php
+++ src/Token.php
@@ -124,7 +124,7 @@ class Token implements AuthToken
         return (new self())->requestToken();
     }
 
-    public function refresh(): static
+    public function refresh(): self
     {
         return $this->requestToken();
     }
